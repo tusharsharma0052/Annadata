14a7838501b18529c5d32d62bc7eb11e5cdb6ca5
tushar29sharma4848-ai <tushar29sharma4848@gmail.com>
2025-12-23 16:40:08 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Frontend URL
+  origin: 'hhttps://annnadata.netlify.app', // Frontend URL
   credentials: true
 }));
 app.use(express.json());
